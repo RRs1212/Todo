2edbbde8775e11a0de48bf9111cfd55c4a3d4c05
Rajasekhar <ravirajasekhar12345@gmail.com>
2024-04-04 02:51:32 +0530

    todo

---
+++ src/main/java/com/example/demo/FAkeTodoservice.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class FAkeTodoservice implements Todoservice{
+    @Override
+    public void dosomething() {
+
+        System.out.println("doing something fake");
+
+    }
+}
